04a77090b728faca32ff14b547705273a0867bbc
Iskarr <32428946+Iskarr@users.noreply.github.com>
2022-02-01 09:12:08 -0700

    added group and milestone project.

---
+++ week05/milestone_project/script.js
@@ -0,0 +1,9 @@
+//todo: Show a list of tasks
+//todo: Add a new task
+//todo: Complete a task
+//todo: Remove a task
+//todo: Filter tasks (complete/incomplete)
+//todo: Data sources: localStorage
+// todo: { id : timestamp, content: string, completed: bool }
+// toDoList = [toDo];
+//todo: Initial Module list: ToDos.js, localStorage helpers (ls.js), utilities.js
