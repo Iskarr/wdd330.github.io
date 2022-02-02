4e189722f48b2773fd17dfcaf04302b4e8be0599
Iskarr <32428946+Iskarr@users.noreply.github.com>
2022-02-02 12:57:23 -0700

    group project

--- week05/milestone_project/script.js
+++ week05/milestone_project/script.js
@@ -7,3 +7,23 @@
 // todo: { id : timestamp, content: string, completed: bool }
 // toDoList = [toDo];
 //todo: Initial Module list: ToDos.js, localStorage helpers (ls.js), utilities.js
+
+// Search function
+const searchBar = document.forms["search-books"].querySelector("input");
+searchBar.addEventListener("keyup", function (e) {
+  const term = e.target.value.toLocaleLowerCase();
+  const searchTag = document.getElementsByTagName("li");
+  let notAvailable = document.getElementById("notAvailable");
+
+  let hasResults = false;
+  Array.from(searchTag).forEach(function (searchTag) {
+    const title = searchTag.textContent;
+    if (title.toLowerCase().indexOf(term) != -1) {
+      searchTag.style.display = "";
+      hasResults = true;
+    } else {
+      searchTag.style.display = "none";
+    }
+  });
+  notAvailable.style.display = hasResults ? "none" : "block";
+});
